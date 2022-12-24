012f1d5749b9f2669e2976b6c7190469aa785109
vitalypolishchuk <vetal.polishuk@gmail.com>
2022-12-24 09:37:39 +0200

    scss set-up

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const { src, dest, watch, series } = require("gulp");
+
+const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
+
+function buildStyles() {
+  return src("sass/**/*.scss").pipe(sass()).pipe(dest("css"));
+}
+
+function watchTask() {
+  watch(["sass/**/*.scss"], buildStyles);
+}
+
+exports.default = series(buildStyles, watchTask);
