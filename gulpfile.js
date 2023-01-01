b4ab344b334981005a8448d9b8b7a9ac6ec22ab7
vitalypolishchuk <vetal.polishuk@gmail.com>
2023-01-01 18:32:47 +0200

    header completed

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,17 @@ const { src, dest, watch, series } = require("gulp");
 
 const sass = require("gulp-sass")(require("sass"));
 const purgecss = require("gulp-purgecss");
+const autoprefixer = require("gulp-autoprefixer");
 
 function buildStyles() {
-  return src("src/sass/**/*.scss").pipe(sass()).pipe(dest("src/css"));
+  return src("src/sass/**/*.scss")
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(dest("src/css"));
 }
 
 function watchTask() {
