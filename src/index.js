d4b674afe0964a3c051eff033ffd0e0d8061ee44
vitalypolishchuk <vetal.polishuk@gmail.com>
2023-01-17 20:29:49 +0200

    section feature

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+const composition = document.querySelector(".composition");
+const compositionPhoto = document.querySelectorAll(".composition__photo");
+console.log(compositionPhoto);
 
+composition.addEventListener("click", (e) => {
+  if (!e.target.classList.contains("composition__photo")) return;
+  [...compositionPhoto].forEach((photo) => {
+    if (photo.classList === e.target.classList) photo.style.zIndex = "100";
+    else photo.style.zIndex = "1";
+  });
+  e.target.style.zIndex = "100";
+});
